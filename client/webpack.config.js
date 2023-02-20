310a5feb36b5eac0f2a30acf062e5203f0af71ca
david <davidmovsichoff@gmail.com>
2023-02-20 15:44:45 -0300

    fix: nested routes does not work because a webpack-dev-server default behaviour
    explanation: https://stackoverflow.com/questions/56573363/react-router-v4-nested-routes-not-work-with-webpack-dev-server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env) => {
 			path: path.join(__dirname, '/dist'),
 			filename: '[name].bundle.js',
 			sourceMapFilename: "[name].js.map",
+			publicPath: '/'
 		},
 
 		devtool: "source-map",
