476fe7a36c4b80bef5efcb8e1bcbcd8f5a43f371
david <davidmovsichoff@gmail.com>
2023-06-07 11:04:42 -0300

    user validation functions in AuthProvider

--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,13 +1,21 @@
 import * as React from 'react'
+import {roles} from '../helpers/roles'
 
 export const AuthContext = React.createContext()
 
 export default function AuthProvider({children}) {
 
-    const [user, setUser] = React.useState(null)
+     const [user, setUser] = React.useState(null)
+    // const [user, setUser] = React.useState({id: 1, role: roles.regular})
+    // const [user, setUser] = React.useState({id: 1, role: roles.admin})
+
+    const isAuthenticated = () => !!user
+    const hasRole = role => !role || user?.role === role
 
     const contextValue = {
-        user
+        user,
+        isAuthenticated,
+        hasRole
     }
 
     return (
