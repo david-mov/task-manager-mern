278e9a93fea8de987316424b1ea65327c2997c97
david <davidmovsichoff@gmail.com>
2023-06-08 12:13:31 -0300

    not found page layout

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import {routes} from '../helpers/routes';
 
 export default function NotFoundPage() {
     return (
-        <div>
-            <h1>Page not found</h1>
-        </div>
+        <Container>
+            <Row className="mt-5">
+                <Col md={{span: 6, offset: 3}} className='text-center'>
+                    <img 
+                        className= "w-100 p-3" 
+                        src="/img/404-not-found.svg" 
+                        alt="error 404"
+                    />
+                    <h2>Page not found</h2>
+                    <p>
+                        Back to <Link to={routes.home}>Home</Link>
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
