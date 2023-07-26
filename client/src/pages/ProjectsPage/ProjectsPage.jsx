615a65956fd348f7eb9598cd9fcf2f4d3f3c76f2
david <davidmovsichoff@gmail.com>
2023-07-26 17:24:41 -0300

    feat: pagination bar

--- client/src/pages/ProjectsPage/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button, Pagination } from 'react-bootstrap'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { compareDate } from '../../helpers/compareDate'
 import useModal from '../../hooks/useModal'
 import CreateProjectModal from './components/CreateProjectModal'
@@ -11,90 +12,106 @@ export default function ProjectsPage() {
     closeCreateProjectModal,
   ] = useModal()
 
-  /*
-  const fakeCards = [
-    {
-      id: 1,
-      title: 'card 1',
-      description: 'description 1',
-      lastUpdate: new Date('2023/02/07 15:13:06'),
-    },
-    {
-      id: 2,
-      title: 'card 2',
-      description: 'description 2',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 3,
-      title: 'card 3',
-      description: 'description 3',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 4,
-      title: 'card 4',
-      description: 'description 4',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 5,
-      title: 'card 5',
-      description: 'description 5',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 6,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 7,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 8,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 9,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 10,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 11,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 12,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-    {
-      id: 13,
-      title: 'card 6',
-      description: 'description 6',
-      lastUpdate: new Date('2011/02/07 15:13:06'),
-    },
-  ]
-  */
+  const getProjects = ({ page }) => {
+    return {
+      docs: [
+        {
+          id: 1,
+          title: 'card 1',
+          description: 'description 1',
+          lastUpdate: new Date('2023/02/07 15:13:06'),
+        },
+        {
+          id: 2,
+          title: 'card 2',
+          description: 'description 2',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 3,
+          title: 'card 3',
+          description: 'description 3',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 4,
+          title: 'card 4',
+          description: 'description 4',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 5,
+          title: 'card 5',
+          description: 'description 5',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 6,
+          title: 'card 6',
+          description: 'description 6',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 7,
+          title: 'card 7',
+          description: 'description 7',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 8,
+          title: 'card 8',
+          description: 'description 8',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 9,
+          title: 'card 9',
+          description: 'description 9',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 10,
+          title: 'card 10',
+          description: 'description 10',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 11,
+          title: 'card 11',
+          description: 'description 11',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+        {
+          id: 12,
+          title: 'card 12',
+          description: 'description 12',
+          lastUpdate: new Date('2011/02/07 15:13:06'),
+        },
+      ],
+      page: 1,
+      totalPages: 10,
+      hasPrevPage: false,
+      hasNextPage: true,
+      prevPage: null,
+      nextPage: 2,
+    }
+  }
 
-  const fakeCards = []
+  const location = useLocation()
+  const query = new URLSearchParams(location.search)
+
+  const requestedPage = parseInt(query.get('page')) || 1
+  const projects = getProjects({ page: requestedPage })
+
+  const navigate = useNavigate()
+
+  function handlePagination({ page: pageNumber }) {
+    return navigate({
+      pathname: '/projects',
+      search: `?page=${pageNumber}`,
+    })
+  }
+
   return (
     <>
       <Container
@@ -112,35 +129,120 @@ export default function ProjectsPage() {
             </div>
           </Col>
         </Row>
-        <Row>
-          {fakeCards.length ? (
-            fakeCards.map((project) => (
-              <Col
-                key={project.id}
-                xs={{ span: 12 }}
-                sm={{ span: 6 }}
-                md={{ span: 4 }}
-                lg={{ span: 3 }}
-              >
-                <Card className='m-2'>
-                  <Card.Img
-                    variant='top'
-                    src='img/projects-card.svg'
-                    className='p-3'
-                  />
-                  <Card.Body>
-                    <Card.Title>{project.title}</Card.Title>
-                    <Card.Text>{project.description}</Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <small className='text-muted'>
-                      Last updated {compareDate(project.lastUpdate).text} ago
-                    </small>
-                  </Card.Footer>
-                </Card>
+        {projects.docs.length ? (
+          <>
+            <Row>
+              {projects.docs.map((project) => (
+                <Col
+                  key={project.id}
+                  xs={{ span: 12 }}
+                  sm={{ span: 6 }}
+                  md={{ span: 4 }}
+                  lg={{ span: 3 }}
+                >
+                  <Card className='m-2'>
+                    <Card.Img
+                      variant='top'
+                      src='img/projects-card.svg'
+                      className='p-3'
+                    />
+                    <Card.Body>
+                      <Card.Title>{project.title}</Card.Title>
+                      <Card.Text>{project.description}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className='text-muted'>
+                        Last updated {compareDate(project.lastUpdate).text} ago
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+            <Row>
+              <Col className='d-flex justify-content-center mt-3'>
+                <Pagination className='m-0'>
+                  {projects.hasPrevPage && (
+                    <Pagination.Prev
+                      onClick={() =>
+                        handlePagination({ page: projects.prevPage })
+                      }
+                    />
+                  )}
+                  {projects.page > 3 && (
+                    <Pagination.Item
+                      onClick={() => handlePagination({ page: 1 })}
+                    >
+                      {1}
+                    </Pagination.Item>
+                  )}
+                  {projects.page > 4 && <Pagination.Ellipsis />}
+
+                  {projects.page > 2 && (
+                    <Pagination.Item
+                      onClick={() =>
+                        handlePagination({ page: projects.page - 2 })
+                      }
+                    >
+                      {projects.page - 2}
+                    </Pagination.Item>
+                  )}
+                  {projects.hasPrevPage && (
+                    <Pagination.Item
+                      onClick={() =>
+                        handlePagination({ page: projects.prevPage })
+                      }
+                    >
+                      {projects.prevPage}
+                    </Pagination.Item>
+                  )}
+
+                  <Pagination.Item active>{projects.page}</Pagination.Item>
+
+                  {projects.hasNextPage && (
+                    <Pagination.Item
+                      onClick={() =>
+                        handlePagination({ page: projects.nextPage })
+                      }
+                    >
+                      {projects.nextPage}
+                    </Pagination.Item>
+                  )}
+                  {projects.totalPages > projects.page + 1 && (
+                    <Pagination.Item
+                      onClick={() =>
+                        handlePagination({ page: projects.page + 2 })
+                      }
+                    >
+                      {projects.page + 2}
+                    </Pagination.Item>
+                  )}
+
+                  {projects.totalPages > projects.page + 3 && (
+                    <Pagination.Ellipsis />
+                  )}
+                  {projects.totalPages > projects.page + 2 && (
+                    <Pagination.Item
+                      onClick={() =>
+                        handlePagination({ page: projects.totalPages })
+                      }
+                    >
+                      {projects.totalPages}
+                    </Pagination.Item>
+                  )}
+                  {projects.hasNextPage && (
+                    <Pagination.Next
+                      onClick={() =>
+                        handlePagination({ page: projects.nextPage })
+                      }
+                    />
+                  )}
+                </Pagination>
               </Col>
-            ))
-          ) : (
+            </Row>
+          </>
+        ) : (
+          <Row>
             <Col md={{ span: 6, offset: 3 }} className='text-center'>
               <img
                 className='w-100 p-3'
@@ -153,8 +255,8 @@ export default function ProjectsPage() {
                 application
               </p>
             </Col>
-          )}
-        </Row>
+          </Row>
+        )}
       </Container>
       <CreateProjectModal
         isOpen={isOpenCreateProjectModal}
