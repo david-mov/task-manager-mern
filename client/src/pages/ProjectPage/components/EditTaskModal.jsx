7ab17f4550c1a3c2a1da95a29f6c6d81cd31a3d6
david <davidmovsichoff@gmail.com>
2023-07-28 13:01:52 -0300

    edit task modal layout

---
+++ client/src/pages/ProjectPage/components/EditTaskModal.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Modal, Form, Button, Alert } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import editTaskResolver from '../../../validations/editTaskResolver'
+import { prioritiesArray } from '../../../helpers/taskPriorities'
+
+const EditTaskModal = ({ isOpen, handleClose }) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: editTaskResolver,
+    defaultValues: {
+      title: 'asgsad',
+      description: 'adsadasf',
+      status: 'Completed',
+      dueDate: new Date().toISOString().slice(0, 10),
+      priority: 'Medium',
+    },
+  })
+
+  const onSubmit = (formData) => {
+    // Workaround ─ HTTP request to the back-end
+    console.log(formData)
+    handleClose()
+  }
+
+  React.useEffect(() => {
+    if (!isOpen) reset()
+  }, [isOpen])
+
+  return (
+    <Modal show={isOpen} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit task</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              placeholder='Change the title'
+              {...register('title')}
+            />
+            {errors.title ? (
+              <Form.Text>
+                <Alert variant='danger'>{errors.title.message}</Alert>
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              placeholder='Change the description'
+              {...register('description')}
+            />
+            {errors.description ? (
+              <Form.Text>
+                <Alert variant='danger'>{errors.description.message}</Alert>
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Due date</Form.Label>
+            <Form.Control
+              type='date'
+              pattern='\d{4}-\d{2}-\d{2}'
+              placeholder='yyyy-mm-dd'
+              {...register('dueDate')}
+            />
+            {errors.dueDate ? (
+              <Form.Text>
+                <Alert variant='danger'>{errors.dueDate.message}</Alert>
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Priority level</Form.Label>
+            {prioritiesArray.map((priority) => (
+              <Form.Check
+                type='radio'
+                name='priority-level'
+                key={priority}
+                label={priority}
+                value={priority}
+                {...register('priority')}
+              />
+            ))}
+            {errors.priority ? (
+              <Form.Text>
+                <Alert variant='danger'>{errors.priority.message}</Alert>
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleSubmit(onSubmit)}>Edit task</Button>
+        <Button variant='secondary' onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+EditTaskModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+}
+
+export default EditTaskModal
---
+++ client/src/validations/editTaskResolver.js
@@ -0,0 +1,21 @@
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { prioritiesArray } from '../helpers/taskPriorities'
+
+const validatePriorityRegex = new RegExp(prioritiesArray.join('|'), 'gi')
+
+const schema = yup.object().shape({
+  title: yup
+    .string('The title must be a text')
+    .required('The title field cannot be left empty'),
+  description: yup.string('The description must be a text'),
+  dueDate: yup.date('The due date must be a valid date'),
+  priority: yup
+    .string('The priority level must be a text')
+    .matches(validatePriorityRegex, {
+      message: 'You must select a valid priority level',
+    })
+    .required('The priority level field cannot be left empty'),
+})
+
+export default yupResolver(schema)
