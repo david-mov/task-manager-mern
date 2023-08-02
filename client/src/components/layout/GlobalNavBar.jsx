05bc17008f02dbd9e4c139d737884e2a7f12e683
david <davidmovsichoff@gmail.com>
2023-08-02 11:09:57 -0300

    fix: fixed navbar spacing

--- client/src/components/layout/GlobalNavBar.jsx
+++ client/src/components/layout/GlobalNavBar.jsx
@@ -10,48 +10,50 @@ export default function GlobalNavBar() {
   const { signout } = useAuthApi()
 
   return (
-    <Navbar collapseOnSelect expand='lg' variant='dark' bg='dark' fixed='top'>
-      <Navbar.Brand as={NavLink} to={routes.home}>
-        Task Manager
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='me-auto'>
-          {isAuthenticated() ? (
-            <Nav.Link as={NavLink} to={routes.projects}>
-              Projects
-            </Nav.Link>
-          ) : null}
-          {isAuthenticated() && hasRole('admin') ? (
-            <NavDropdown title='Admin'>
-              <NavDropdown.Item as={NavLink} to={routes.admin.users}>
-                Users
-              </NavDropdown.Item>
-            </NavDropdown>
-          ) : null}
-        </Nav>
-        <Nav>
-          {isAuthenticated() ? (
-            <>
-              <Nav.Link as={NavLink} to={routes.account}>
-                Account
+    <div style={{ marginBottom: '5rem' }}>
+      <Navbar collapseOnSelect expand='lg' variant='dark' bg='dark' fixed='top'>
+        <Navbar.Brand as={NavLink} to={routes.home}>
+          Task Manager
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            {isAuthenticated() ? (
+              <Nav.Link as={NavLink} to={routes.projects}>
+                Projects
               </Nav.Link>
-              <Nav.Link onClick={signout} to={routes.home}>
-                Sign-out
-              </Nav.Link>
-            </>
-          ) : (
-            <>
-              <Nav.Link as={NavLink} to={routes.signin}>
-                Sign-in
-              </Nav.Link>
-              <Nav.Link as={NavLink} to={routes.signup}>
-                Sign-up
-              </Nav.Link>
-            </>
-          )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            ) : null}
+            {isAuthenticated() && hasRole('admin') ? (
+              <NavDropdown title='Admin'>
+                <NavDropdown.Item as={NavLink} to={routes.admin.users}>
+                  Users
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : null}
+          </Nav>
+          <Nav>
+            {isAuthenticated() ? (
+              <>
+                <Nav.Link as={NavLink} to={routes.account}>
+                  Account
+                </Nav.Link>
+                <Nav.Link onClick={signout} to={routes.home}>
+                  Sign-out
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={NavLink} to={routes.signin}>
+                  Sign-in
+                </Nav.Link>
+                <Nav.Link as={NavLink} to={routes.signup}>
+                  Sign-up
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
